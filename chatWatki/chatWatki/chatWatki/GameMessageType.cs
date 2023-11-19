8ddbeb8eff4c9eb91181b12c13d52f78d8bc2769
OskarJaszczor <oskarjaszczor@gmail.com>
2023-11-19 14:10:23 +0100

    123
    213

---
+++ chatWatki/chatWatki/chatWatki/GameMessageType.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace chatWatki
+{
+    enum GameMessageType
+    {
+        Chat,
+        TicTacToeMove,
+        TicTacToeWin,
+        TicTacToeDraw,
+        TicTacToeReset,
+        IpAdressNick,
+        IsAlive
+    }
+}
---
+++ chatWatki/chatWatki/chatWatki/Lobby.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+
+namespace chatWatki
+{
+    class Lobby
+    {
+        public List<TcpClient> waiting_player = new List<TcpClient>();
+
+        public void addClient(TcpClient client)
+        {
+            waiting_player.Add(client);
+        }
+
+        public void Start()
+        {
+            new Thread(() =>
+            {
+                while(true)
+                {
+                    if(waiting_player.Count >=2)
+                    {
+                        //Create Game
+                        Game gra = new Game();
+                        gra.Start();
+
+                        gra.addClient(waiting_player[0]);
+                        gra.addClient(waiting_player[1]);
+                        waiting_player.RemoveAt(0);
+                        waiting_player.RemoveAt(0);
+                    }
+                }
+            }).Start();
+        }
+
+    }
+}
